d232c939faf937ef9ba35bcfb96f0b470286be4e
John Truong <jot@odoo.com>
2021-07-06 10:51:26 -0400

    [IMP] project_task_scheduler: perf improvements by storing predecessor field

--- spence_project_task_scheduling/models/project.py
+++ spence_project_task_scheduling/models/project.py
@@ -12,7 +12,7 @@ class Task(models.Model):
     date_start = fields.Date("Start Date", readonly=False, default=fields.Date.context_today, store=True, compute="_compute_date_start")
     date_end = fields.Date("End Date", compute="_compute_end_date")
     duration = fields.Integer("Duration in Days", default=1)
-    predecessor = fields.Many2one('project.task', string="Predecessor", compute="_compute_siblings")
+    predecessor = fields.Many2one('project.task', string="Predecessor", compute="_compute_siblings", store=True)
 
     @api.depends('predecessor.date_start','predecessor.duration')
     def _compute_date_start(self):
@@ -24,10 +24,10 @@ class Task(models.Model):
         for task in self:
             task.date_end = task.date_start + timedelta(days=task.duration - 1)
     
-    @api.depends("sequence")
+    @api.depends("sequence","predecessor.sequence")
     def _compute_siblings(self):
         for task in self:
-            earlier_tasks = task.project_id.tasks.filtered(lambda t: t.sequence < task.sequence)
+            earlier_tasks = task.project_id.tasks.filtered(lambda t: t.sequence < task.sequence).sorted('sequence') # For some reason it wasn't automatically being sorted so we explcitly sort here
             task.predecessor = earlier_tasks[-1] if earlier_tasks else None
 
     # I'm not sure why but even though we have editable = 'bottom' on the list view, 
