85c3fd360ca3ce9ea0d29daf43db3a6d694b3e44
John Truong <jot@odoo.com>
2021-07-07 17:14:08 -0400

    [ADD] project_multiple_worksheets

---
+++ spence_project_multiple_worksheets/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Spence Corrosion: Multiple Worksheets",
+
+    'summary': """Allow for multiple worksheets per task""",
+
+    'description': """
+        []
+        """,
+
+    'author': 'Odoo Inc',
+    'website': 'https://www.odoo.com/',
+
+    'category': 'Custom Development',
+    'version': '1.0',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['project'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+    'application': False,
+}
