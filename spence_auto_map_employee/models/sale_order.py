fd281189710ff1157be7f367edb2f2ddba00034d
John Truong <jot@odoo.com>
2021-08-20 14:57:03 -0400

    [IMP] map_employee: labour lines dont necessarily need to create their own task

--- spence_auto_map_employee/models/sale_order.py
+++ spence_auto_map_employee/models/sale_order.py
@@ -9,11 +9,14 @@ class SaleOrder(models.Model):
 
     def action_confirm(self):
         res = super().action_confirm()
+        if self.project_ids:
+            self.project_id = self.project_ids[0]
+        if any(self.mapped('order_line.product_id.is_labour')):
+            self.project_id.pricing_type = 'employee_rate'
         EmployeeMap = self.env['project.sale.line.employee.map']
-        for line in self.order_line.filtered(lambda l: l.product_id.is_labour and l.project_id):
-            line.project_id.pricing_type = 'employee_rate'
+        for line in self.order_line.filtered(lambda l: l.product_id.is_labour):
             EmployeeMap.create({
-                'project_id': line.project_id.id,
+                'project_id': self.project_id.id,
                 'sale_line_id': line.id,
                 'employee_id': line.product_id.labourer_title_id.id,
             })
